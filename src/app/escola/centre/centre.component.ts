879c3ed74244e5c98b2910d38285a7d20cc00608
ITE-JCALLE\jcalle <jcalle@teknos.cat>
2017-06-07 15:49:10 +0200

    Comencat centre

--- src/app/escola/centre/centre.component.ts
+++ src/app/escola/centre/centre.component.ts
@@ -4,30 +4,17 @@ import {CentreService} from './centre.service';
 
 
 @Component({
-<<<<<<< HEAD
- selector: 'centre',
- templateUrl:'./centre.component.html',
- styleUrls: ['../../cotxes.css'],
- providers: [CentreService]
-=======
   selector: 'centre',
   templateUrl:'./centre.component.html',
   styleUrls: ['../../cotxes.css'],
   providers: [CentreService]
->>>>>>> 8f9e917f9ba1563dd96a0f5c65d612e7c36f17f6
 })
 
    export class CentreComponent{
        
 
-<<<<<<< HEAD
           
 constructor(private centreService: CentreService) { } 
        
    }
-=======
            
- constructor(private centreService: CentreService) { } 
-        
-    }
->>>>>>> 8f9e917f9ba1563dd96a0f5c65d612e7c36f17f6
