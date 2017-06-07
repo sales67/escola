6cf0655c29b7a2ad51eb370eef90ce6821aa3ea2
ITE-JCALLE\jcalle <jcalle@teknos.cat>
2017-06-07 09:42:27 +0200

    Base de Centre

---
+++ src/app/escola/centre/centre.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {CentreService} from './centre.service';
+
+
+
+@Component({
+  selector: 'centre',
+  templateUrl:'./centr.component.html',
+  styleUrls: ['../../cotxes.css'],
+  providers: [CentreService]
+})
+
+    export class CentreComponent{
+        
+
+           
+ constructor(private centreService: CentreService) { } 
+
+}
