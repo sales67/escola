324298ffbdd43214047b5d60292f41ddbec522a2
ITE-DCACERES\dcaceres <asesino5>
2017-06-07 11:56:16 +0200

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,13 +9,11 @@ import { AddCursComponent } from './escola/curs/addCurs/addCurs.component';
 import { ListCursComponent } from './escola/curs/listCurs/listCurs.component';
 
 import { ProfessorComponent } from './escola/professor/professor.component';
-<<<<<<< HEAD
+
 import { EsportComponent } from './escola/professor/esport/esport.component';
 import { CrearProfeComponent } from './escola/professor/crear-profe/crear-profe.component';
-=======
-import { EsportComponent } from './escola//professor/esport/esport.component';
-import { CrearProfeComponent } from './escola//professor/crear-profe/crear-profe.component';
->>>>>>> 8f9e917f9ba1563dd96a0f5c65d612e7c36f17f6
+
+
 import { ConsultarProfeComponent } from './escola/professor/consultar-profe/consultar-profe.component';
 import { ModificarProfeComponent } from './escola/professor/modificar-profe/modificar-profe.component';
 import { EsborrarProfeComponent } from './escola/professor/esborrar-profe/esborrar-profe.component';
@@ -25,7 +23,6 @@ const routes: Routes = [
     path: 'alumne',
     component: AlumneComponent,
     children: []
-<<<<<<< HEAD
     },  
   {
       path: 'curs',
@@ -48,8 +45,6 @@ const routes: Routes = [
       path: 'listCurs',
       component: ListCursComponent,
   },   
-=======
-  },
     {
     path: 'professor',
     component: ProfessorComponent,
@@ -75,7 +70,7 @@ const routes: Routes = [
     component: EsborrarProfeComponent,
     children: []
   },
->>>>>>> 089bfaf7324cee0e22e6b5e5fadb84333323a5bb
+
 ];
 
 @NgModule({
--- src/app/escola/escola.module.ts
+++ src/app/escola/escola.module.ts
@@ -5,7 +5,7 @@ import {AppRoutingModule} from '../app-routing.module';
 import {FormsModule} from '@angular/forms';
 
 
-<<<<<<< HEAD
+
 import { AlumneComponent } from './alumne/alumne.component';
 import { CursComponent } from './curs/curs.component';
 import { DelCursComponent } from './curs/delCurs/delCurs.component';
@@ -13,15 +13,14 @@ import { PutCursComponent } from './curs/putCurs/putCurs.component';
 import { AddCursComponent } from './curs/addCurs/addCurs.component';
 import { ListCursComponent } from './curs/listCurs/listCurs.component';
 
-=======
-import {AlumneComponent} from './alumne/alumne.component';
+
 import { ProfessorComponent } from './professor/professor.component';
 import { EsportComponent } from './professor/esport/esport.component';
 import { CrearProfeComponent } from './professor/crear-profe/crear-profe.component';
 import { ConsultarProfeComponent } from './professor/consultar-profe/consultar-profe.component';
 import { ModificarProfeComponent } from './professor/modificar-profe/modificar-profe.component';
 import { EsborrarProfeComponent } from './professor/esborrar-profe/esborrar-profe.component';
->>>>>>> 089bfaf7324cee0e22e6b5e5fadb84333323a5bb
+
 
 @NgModule({
   imports: [
@@ -32,21 +31,21 @@ import { EsborrarProfeComponent } from './professor/esborrar-profe/esborrar-prof
   ],
   declarations: [
       AlumneComponent,
-<<<<<<< HEAD
+
       CursComponent,
       DelCursComponent,
       ListCursComponent,
       AddCursComponent,
-      PutCursComponent
+      PutCursComponent,
       
-=======
+
       ProfessorComponent,
       EsportComponent,
       CrearProfeComponent,
       ConsultarProfeComponent,
       ModificarProfeComponent,
       EsborrarProfeComponent
->>>>>>> 089bfaf7324cee0e22e6b5e5fadb84333323a5bb
+
   ]
 })
 export class EscolaModule { }
