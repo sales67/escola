599643e4de5eeec52265bc963c7abfe639d632be
ITE-JMALAGARRIG\jmalagarriga <r11j14@gmail.com>
2017-06-13 11:49:51 +0200

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,14 @@
+// Las aplicaciones Angular2 están pensadas para crecer, para ello es fundamental cierto grado de modularidad
+
+// Un módulo no es más que una clase contenedora
+
+// Cada módulo puede incluir múltiples componentes y servicios
+
+// Normalmente un módulo dependerá de otros
+
+// El módulo raíz declara un componente especial para el arranque de la aplicación: El componente raíz
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// por último la aplicación a leída desde el módulo raíz, llamado app por convenio
+import { AppModule } from './app/app.module';
+
+// condiciones para ejecutar en modo desarrollo o producción
 if (environment.production) {
   enableProdMode();
 }
 
+// arranque de la aplicación invocando al módulo raíz
 platformBrowserDynamic().bootstrapModule(AppModule);
