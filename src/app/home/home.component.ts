b4ab1e6f6b2835c024116e66b1fee0031ecd227d
ITE-SSALES\ssales <salesvila67@gmail.com>
2017-08-23 14:56:52 +0200

    no message

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from './home.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  //styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.css'],
     providers: [ HomeService ]
 })
 export class HomeComponent {
-    constructor(private homeService: HomeService) { }    
+    //constructor(private homeService: HomeService) { }
+    
+   model: any = {};
+    loading = false;
+    error = '';
+
+    constructor(
+        private router: Router,
+        private homeService: HomeService) { }
+
+    ngOnInit() {
+        // reset login status
+        this.homeService.logout();
+    }
+
+    login() {
+        this.loading = true;
+        this.homeService.login(this.model.username, this.model.password)
+            .subscribe(result => {
+                if (result === true) {
+                    // login successful
+                    //this.router.navigate(['home']);
+                } else {
+                    // login failed
+                    this.error = 'Username or password is incorrect';
+                    this.loading = false;
+                }
+            }, error => {
+              this.loading = false;
+              this.error = error;
+            });
+    }
 }
