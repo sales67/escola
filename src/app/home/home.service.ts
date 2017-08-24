bb2b9a85df8bfa16d6bb0a92d88f0b9036af9326
ITE-SSALES\ssales <salesvila67@gmail.com>
2017-08-24 09:35:34 +0200

    no message

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
+
 @Injectable()
 export class HomeService {
 
     constructor(private http: Http) { }
     private authUrl = 'http://localhost:8080/auth';
-    private headers = new Headers({'Content-Type': 'application/json'});
+    private headers = new Headers({'Content-Type': 'application/json'});    
 
-    login(username: string, password: string): Observable<boolean> {
-        console.log(password);
-        return this.http.post(this.authUrl, JSON.stringify({username: username, password: password}), {headers: this.headers})
-            .map((response: Response) => {
-                // login successful if there's a jwt token in the response
-                let token = response.json() && response.json().token;
+    login(username: string, password: string): Observable<boolean>{
+        
+        let creds = JSON.stringify({'username':username, 'password':password});
+        
+        
+       // headers.append('Content-Type', 'application/x-www-form-urlencoded');         
+        let options = new RequestOptions({headers: this.headers});
+
+        return this.http.post(this.authUrl,creds,options)
+            .map((response: Response) => { 
+                 console.log("dasdada");
+                // login successful if there's a jwt token in the response        
+                let token = response.json().data && response.json().token;
                 if (token) {
                     // store username and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify({ username: username, token: token }));
@@ -28,13 +36,15 @@ export class HomeService {
                     return false;
                 }
             }).catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+            //console.log(a);
+            //return a;
     }
 
-    getToken(): String {
+   /* getToken(): String {
       var currentUser = JSON.parse(localStorage.getItem('currentUser'));
       var token = currentUser && currentUser.token;
       return token ? token : "";
-    }
+    }*/
 
     logout(): void {
         // clear token remove user from local storage to log user out
