83a8d17e7aecbd37c242a4bb76612fd3cd332de2
ITE-JMALAGARRIG\jmalagarriga <r11j14@gmail.com>
2017-08-25 13:58:58 +0200

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { EscolaModule } from './escola/escola.module';
 import { FormulariComponent } from './formulari/formulari.component';
 
+import { LoginComponent } from './login/login.component';
 import { LoginService } from './login/login.service';
 
 @NgModule({
