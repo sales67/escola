7fe21b5e0da8859e7c21430993ffe5f204f953cc
ITE-SSALES\ssales <salesvila67@gmail.com>
2017-08-25 12:56:23 +0200

    no message

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -19,7 +19,6 @@ export class LoginComponent{
     constructor(
         private router: Router,
         private loginService: LoginService) {}
-    //token:any;
 
     ngOnInit() {
         this.loginService.logout(); 
@@ -32,25 +31,10 @@ export class LoginComponent{
             .subscribe(result => {
             console.log(result);
                 if (result === true) {
-<<<<<<< HEAD
-=======
-
->>>>>>> 4d8feb49dec917403be7b4da4c7f05f3522cfc2b
                     console.log("el resultat es true");
-<<<<<<< HEAD:src/app/login/login.component.ts
                     window.location.reload();
-                    this.router.navigate(['consultar-profe']);                    
-=======
-                    this.router.navigate(['/consultar-profe']);
-                    console.log(result);
-                    // login successful
-
-
-
-                    window.location.reload()
-                    this.router.navigate(['consultar-profe']);                   
+                    this.router.navigate(['consultar-profe']);   
                     
->>>>>>> 893d4ddf52b65ccc3547a258c63b98a759f6f07c:src/app/home/home.component.ts
                 } else {
                     console.log("el resultat es false");
                     this.error = 'Username or password is incorrect';
@@ -61,44 +45,3 @@ export class LoginComponent{
               this.error = error;
             });
     }
-    /*login() {
-        this.loading = true;
-        this.homeService.login(this.model.username, this.model.password)
-            .subscribe(result => {
-                if (result === true) {
-
-                    console.log("el resultat es true");
-                    this.router.navigate(['/consultar-profe']);
-                    console.log(result);
-                    // login successful
-                    //this.router.navigate(['home']);
-
-                    console.log("el resultat es true");
-                    this.router.navigate(['/consultar-profe']);
-
-                    console.log(result);
-
-<<<<<<< HEAD
->>>>>>> 122e80dc50d44211b8ec2afad94d8b0fe2c7a0fc
->>>>>>> c5e2a72e68516af98cb3eff83c4fd7d442b9912b
-=======
->>>>>>> 0375a0fc732131a324f0125fd4034ee23406b92f
-                } else {
-                    // login failed                    
-                    this.error = 'Username or password is incorrect';
-                    this.loading = false;
-                }
-                //console.log(result);
-            }, error => {
-              this.loading = false;
-              this.error = error;
-            });
-<<<<<<< HEAD
-<<<<<<< HEAD
-    }*/
-<<<<<<< HEAD
-}
-=======
-}
-
->>>>>>> 4d8feb49dec917403be7b4da4c7f05f3522cfc2b
